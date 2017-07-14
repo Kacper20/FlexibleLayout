7dd471823df0080aefa188aceffa8a284d34f9d7
Kacper Harasim <kacper.harasim@polidea.com>
2017-07-14 19:52:50 +0200

    Initial Commit

---
+++ FlexibleLayout/FlexibleLayout.h
@@ -0,0 +1,19 @@
+//
+//  FlexibleLayout.h
+//  FlexibleLayout
+//
+//  Created by Kacper Harasim on 14/07/2017.
+//  Copyright © 2017 Harasim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlexibleLayout.
+FOUNDATION_EXPORT double FlexibleLayoutVersionNumber;
+
+//! Project version string for FlexibleLayout.
+FOUNDATION_EXPORT const unsigned char FlexibleLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexibleLayout/PublicHeader.h>
+
+
