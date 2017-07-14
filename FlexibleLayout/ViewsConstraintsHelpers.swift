a979f02d65ee44e7980f61c71afbc7ac98f74651
Kacper Harasim <kacper.harasim@polidea.com>
2017-07-14 21:23:17 +0200

    Moved creating constraints for heights to helper method

--- FlexibleLayout/ViewsConstraintsHelpers.swift
+++ FlexibleLayout/ViewsConstraintsHelpers.swift
@@ -9,7 +9,41 @@
 import Foundation
 
 struct ConstraintsHelpers {
+
+    private static func activateConstraint(
+        fromView: UIView,
+        toView: UIView,
+        fromAttribute: NSLayoutAttribute,
+        toAttribute: NSLayoutAttribute
+        ) {
+        NSLayoutConstraint(
+            item: fromView,
+            attribute: fromAttribute,
+            relatedBy: .equal,
+            toItem: toView,
+            attribute: toAttribute,
+            multiplier: 1.0,
+            constant: 0.0
+        ).isActive = true
+    }
+
     private static func createHeightEnforcingViewAndSetupConstraints(
+        for space: VerticalFlexibleSpace,
+        in container: UIView
+        ) -> UIView {
+        let view = UIView()
+        container.addSubview(view)
+        view.backgroundColor = .clear
+        let from = space.from
+        let to = space.to
+        activateConstraint(fromView: view, toView: container, fromAttribute: .width, toAttribute: .width)
+        activateConstraint(fromView: view, toView: space.from.view, fromAttribute: .centerX, toAttribute: .centerX)
+        activateConstraint(fromView: view, toView: from.view, fromAttribute: .top, toAttribute: from.attribute)
+        activateConstraint(fromView: view, toView: to.view, fromAttribute: .bottom, toAttribute: to.attribute)
+        return view
+    }
+
+    private static func createWidthEnforcingViewAndSetupConstraints(
         for space: VerticalFlexibleSpace,
         in container: UIView
         ) -> UIView {
@@ -25,7 +59,7 @@ struct ConstraintsHelpers {
             attribute: .width,
             multiplier: 1.0,
             constant: 0.0
-        ).isActive = true
+            ).isActive = true
 
         NSLayoutConstraint(
             item: view,
@@ -35,7 +69,7 @@ struct ConstraintsHelpers {
             attribute: .centerX,
             multiplier: 1.0,
             constant: 0.0
-        ).isActive = true
+            ).isActive = true
 
         NSLayoutConstraint(
             item: view,
@@ -45,7 +79,7 @@ struct ConstraintsHelpers {
             attribute: space.from.attribute,
             multiplier: 1.0,
             constant: 0.0
-        ).isActive = true
+            ).isActive = true
 
         NSLayoutConstraint(
             item: view,
@@ -55,7 +89,7 @@ struct ConstraintsHelpers {
             attribute: space.to.attribute,
             multiplier: 1.0,
             constant: 0.0
-        ).isActive = true
+            ).isActive = true
 
         NSLayoutConstraint(
             item: view,
@@ -65,7 +99,7 @@ struct ConstraintsHelpers {
             attribute: .width,
             multiplier: 1.0,
             constant: 0.0
-        ).isActive = true
+            ).isActive = true
         
         return view
     }
