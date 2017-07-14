f35d50868688003546dccd0e6f6f7426b7dbe8c2
Kacper Harasim <kacper.harasim@polidea.com>
2017-07-14 20:26:42 +0200

    Divided layout targets into two files for visibility & clarity

--- FlexibleLayout/LayoutXTarget.swift
+++ FlexibleLayout/LayoutXTarget.swift
@@ -1,5 +1,5 @@
 //
-//  LayoutTargetY.swift
+//  LayoutXTarget.swift
 //  FlexibleLayout
 //
 //  Created by Kacper Harasim on 14/07/2017.
@@ -59,55 +59,6 @@ public struct LayoutXTarget {
         case .leadingMargin: return view.layoutMarginsGuide.leadingAnchor
         case .trailingMargin: return view.layoutMarginsGuide.trailingAnchor
         case .centerXWithinMargins: return view.layoutMarginsGuide.centerXAnchor
-        }
-    }
-}
-
-struct LayoutYTarget {
-
-    let view: UIView
-    let kind: Kind
-    
-    enum Kind {
-        case top
-        case bottom
-        case centerY
-        case topMargin
-        case bottomMargin
-        case centerYWithinMargins
-        case firstBaseline
-        case lastBaseline
-    }
-
-    init(kind: Kind, view: UIView) {
-        self.kind = kind
-        self.view = view
-    }
-
-    var attribute: NSLayoutAttribute {
-        switch self.kind {
-        case .top: return .top
-        case .bottom: return .bottom
-        case .centerY: return .centerY
-        case .topMargin: return .topMargin
-        case .bottomMargin: return .bottomMargin
-        case .centerYWithinMargins: return .centerYWithinMargins
-        case .firstBaseline: return .firstBaseline
-        case .lastBaseline: return .lastBaseline
-        }
-    }
-
-    @available(iOS 9.0, *)
-    var anchor: NSLayoutYAxisAnchor {
-        switch self.kind {
-        case .top: return view.topAnchor
-        case .bottom: return view.bottomAnchor
-        case .centerY: return view.centerYAnchor
-        case .topMargin: return view.layoutMarginsGuide.topAnchor
-        case .bottomMargin: return view.layoutMarginsGuide.bottomAnchor
-        case .centerYWithinMargins: return view.layoutMarginsGuide.centerYAnchor
-        case .firstBaseline: return view.firstBaselineAnchor
-        case .lastBaseline: return view.lastBaselineAnchor
         }
     }
 }
---
+++ FlexibleLayout/LayoutYTarget.swift
@@ -0,0 +1,58 @@
+//
+//  LayoutYTarget.swift
+//  FlexibleLayout
+//
+//  Created by Kacper Harasim on 14/07/2017.
+//  Copyright © 2017 Harasim. All rights reserved.
+//
+
+import Foundation
+
+struct LayoutYTarget {
+
+    let view: UIView
+    let kind: Kind
+
+    enum Kind {
+        case top
+        case bottom
+        case centerY
+        case topMargin
+        case bottomMargin
+        case centerYWithinMargins
+        case firstBaseline
+        case lastBaseline
+    }
+
+    init(kind: Kind, view: UIView) {
+        self.kind = kind
+        self.view = view
+    }
+
+    var attribute: NSLayoutAttribute {
+        switch self.kind {
+        case .top: return .top
+        case .bottom: return .bottom
+        case .centerY: return .centerY
+        case .topMargin: return .topMargin
+        case .bottomMargin: return .bottomMargin
+        case .centerYWithinMargins: return .centerYWithinMargins
+        case .firstBaseline: return .firstBaseline
+        case .lastBaseline: return .lastBaseline
+        }
+    }
+
+    @available(iOS 9.0, *)
+    var anchor: NSLayoutYAxisAnchor {
+        switch self.kind {
+        case .top: return view.topAnchor
+        case .bottom: return view.bottomAnchor
+        case .centerY: return view.centerYAnchor
+        case .topMargin: return view.layoutMarginsGuide.topAnchor
+        case .bottomMargin: return view.layoutMarginsGuide.bottomAnchor
+        case .centerYWithinMargins: return view.layoutMarginsGuide.centerYAnchor
+        case .firstBaseline: return view.firstBaselineAnchor
+        case .lastBaseline: return view.lastBaselineAnchor
+        }
+    }
+}
