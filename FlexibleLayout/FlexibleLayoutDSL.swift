47455966946bd2cf80030e3bebb321544b95465c
Kacper Harasim <kacper.harasim@polidea.com>
2017-07-15 12:35:21 +0200

    Changed the way layout targets are obtained to DSL object

---
+++ FlexibleLayout/FlexibleLayoutDSL.swift
@@ -0,0 +1,85 @@
+//
+//  FlexibleLayoutDSL.swift
+//  FlexibleLayout
+//
+//  Created by Kacper Harasim on 15/07/2017.
+//  Copyright © 2017 Harasim. All rights reserved.
+//
+
+import Foundation
+
+public struct FlexibleLayoutDSL {
+    let view: UIView
+
+    public var left: LayoutXTarget {
+        return LayoutXTarget(kind: .left, view: view)
+    }
+
+    public var right: LayoutXTarget {
+        return LayoutXTarget(kind: .right, view: view)
+    }
+
+    public var leading: LayoutXTarget {
+        return LayoutXTarget(kind: .leading, view: view)
+    }
+
+    public var trailing: LayoutXTarget {
+        return LayoutXTarget(kind: .trailing, view: view)
+    }
+
+    public var centerX: LayoutXTarget {
+        return LayoutXTarget(kind: .centerX, view: view)
+    }
+
+    public var leftMargin: LayoutXTarget {
+        return LayoutXTarget(kind: .leftMargin, view: view)
+    }
+
+    public var rightMargin: LayoutXTarget {
+        return LayoutXTarget(kind: .rightMargin, view: view)
+    }
+
+    public var leadingMargin: LayoutXTarget {
+        return LayoutXTarget(kind: .leadingMargin, view: view)
+    }
+
+    public var trailingMargin: LayoutXTarget {
+        return LayoutXTarget(kind: .trailingMargin, view: view)
+    }
+
+    public var centerXWithinMargins: LayoutXTarget {
+        return LayoutXTarget(kind: .centerXWithinMargins, view: view)
+    }
+
+    public var top: LayoutYTarget {
+        return LayoutYTarget(kind: .top, view: view)
+    }
+
+    public var bottom: LayoutYTarget {
+        return LayoutYTarget(kind: .bottom, view: view)
+    }
+
+    public var centerY: LayoutYTarget {
+        return LayoutYTarget(kind: .centerY, view: view)
+    }
+
+    public var topMargin: LayoutYTarget {
+        return LayoutYTarget(kind: .topMargin, view: view)
+    }
+
+    public var bottomMargin: LayoutYTarget {
+        return LayoutYTarget(kind: .bottomMargin, view: view)
+    }
+
+    public var centerYWithinMargins: LayoutYTarget {
+        return LayoutYTarget(kind: .centerYWithinMargins, view: view)
+    }
+
+    public var firstBaseline: LayoutYTarget {
+        return LayoutYTarget(kind: .firstBaseline, view: view)
+    }
+
+    public var lastBaseline: LayoutYTarget {
+        return LayoutYTarget(kind: .lastBaseline, view: view)
+    }
+}
--- FlexibleLayout/View+LayoutTargets.swift
+++ FlexibleLayout/View+LayoutTargets.swift
@@ -8,77 +8,8 @@
 
 import Foundation
 
-extension UIView {
-
-    public var leftTarget: LayoutXTarget {
-        return LayoutXTarget(kind: .left, view: self)
-    }
-
-    public var rightTarget: LayoutXTarget {
-        return LayoutXTarget(kind: .right, view: self)
-    }
-
-    public var leadingTarget: LayoutXTarget {
-        return LayoutXTarget(kind: .leading, view: self)
-    }
-
-    public var trailingTarget: LayoutXTarget {
-        return LayoutXTarget(kind: .trailing, view: self)
-    }
-
-    public var centerXTarget: LayoutXTarget {
-        return LayoutXTarget(kind: .centerX, view: self)
-    }
-
-    public var leftMarginTarget: LayoutXTarget {
-        return LayoutXTarget(kind: .leftMargin, view: self)
-    }
-
-    public var rightMarginTarget: LayoutXTarget {
-        return LayoutXTarget(kind: .rightMargin, view: self)
-    }
-
-    public var leadingMarginTarget: LayoutXTarget {
-        return LayoutXTarget(kind: .leadingMargin, view: self)
-    }
-
-    public var trailingMarginTarget: LayoutXTarget {
-        return LayoutXTarget(kind: .trailingMargin, view: self)
-    }
-
-    public var centerXWithinMarginsTarget: LayoutXTarget {
-        return LayoutXTarget(kind: .centerXWithinMargins, view: self)
-    }
-
-    public var topTarget: LayoutYTarget {
-        return LayoutYTarget(kind: .top, view: self)
-    }
-
-    public var bottomTarget: LayoutYTarget {
-        return LayoutYTarget(kind: .bottom, view: self)
-    }
-
-    public var centerYTarget: LayoutYTarget {
-        return LayoutYTarget(kind: .centerY, view: self)
-    }
-
-    public var topMarginTarget: LayoutYTarget {
-        return LayoutYTarget(kind: .topMargin, view: self)
-    }
-
-    public var bottomMarginTarget: LayoutYTarget {
-        return LayoutYTarget(kind: .bottomMargin, view: self)
-    }
-
-    public var centerYWithinMarginsTarget: LayoutYTarget {
-        return LayoutYTarget(kind: .centerYWithinMargins, view: self)
-    }
-
-    public var firstBaselineTarget: LayoutYTarget {
-        return LayoutYTarget(kind: .firstBaseline, view: self)
-    }
-
-    public var lastBaselineTarget: LayoutYTarget {
-        return LayoutYTarget(kind: .lastBaseline, view: self)
+public extension UIView {
+    public var flx: FlexibleLayoutDSL {
+        return FlexibleLayoutDSL(view: self)
     }
 }
